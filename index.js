08e5bc901d1b2b08f39ba0f5fcb824c6fde61447
Max Zimmerman <william.m.zimmerman@gmail.com>
2018-03-02 19:15:26 -0600

    Started the scraping for TCG will need to switch to something that will process JS

--- index.js
+++ index.js
@@ -10,88 +10,116 @@ app.use(express.static(__dirname + '/public'));
 var bodyParser = require('body-parser');
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-    extended: true
+  extended: true
 }));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-    res.render('pages/index');
+  res.render('pages/index');
 });
 
 app.get('/mainstreetmagic', function(req, res) {
-    res.render('pages/mainstreetmagic');
+  res.render('pages/mainstreetmagic');
 });
 
 app.get('/msmWantCards', function(req, res){
-    //Web SCrapping done here
-    var url = 'http://www.msmmm.com/wantlist';
+  //Web SCrapping done here
+  var url = 'http://www.msmmm.com/wantlist';
 
-    request(url, function(error, response, html){
-        if(!error){
-            var $ = cheerio.load(html);
+  request(url, function(error, response, html){
+    if(!error){
+      var $ = cheerio.load(html);
 
-            var content = $('.view-content');
-            var children = content.children();
-            var sets = content.find('h3');
-            var tables = content.find('table');
-            var setsLength = sets.length;
-            var tablesLength = tables.length;
-            var cards = [];
+      var content = $('.view-content');
+      var children = content.children();
+      var sets = content.find('h3');
+      var tables = content.find('table');
+      var setsLength = sets.length;
+      var tablesLength = tables.length;
+      var cards = [];
 
-            for(var i = 0; i < setsLength; i++){
-                var setName = $(sets[i]).text();
-                var rows = $(tables[i]).find('tr');
-                var rowLength = rows.length;
+      for(var i = 0; i < setsLength; i++){
+        var setName = $(sets[i]).text();
+        var rows = $(tables[i]).find('tr');
+        var rowLength = rows.length;
 
-                for(var j = 0; j < rowLength; j++){
-                    var cols = $(rows[j]).find('td');
-                    var colLength = cols.length;
+        for(var j = 0; j < rowLength; j++){
+          var cols = $(rows[j]).find('td');
+          var colLength = cols.length;
 
-                    for(var x = 0; x < colLength; x++){
-                        var cardName = $(cols[x]).children().first().children().first().text();
-                        var cardCost = $(cols[x]).children().last().children().first().text();
+          for(var x = 0; x < colLength; x++){
+            var cardName = $(cols[x]).children().first().children().first().text();
+            var cardCost = $(cols[x]).children().last().children().first().text();
 
-                        cards.push({ name: cardName, set: setName, cost: cardCost });
-                    }
-                }
-            }
-
-            res.send(JSON.stringify(cards));
+            cards.push({ name: cardName, set: setName, cost: cardCost });
+          }
         }
-    });
+      }
+
+      res.send(JSON.stringify(cards));
+    }
+  });
 });
 
 app.get('/msmSets', function(req, res){
-    //Web SCrapping done here
-    var url = 'http://www.msmmm.com/pricelist';
+  //Web SCrapping done here
+  var url = 'http://www.msmmm.com/pricelist';
 
-    request(url, function(error, response, html){
-        if(!error){
-            var $ = cheerio.load(html);
-            var content = $('#edit-field-magic-setname-nid');
-            var sets = content.find('option');
-            var setsLength = sets.length;
-            var setList = [];
+  request(url, function(error, response, html){
+    if(!error){
+      var $ = cheerio.load(html);
+      var content = $('#edit-field-magic-setname-nid');
+      var sets = content.find('option');
+      var setsLength = sets.length;
+      var setList = [];
 
-            for(var i = 0; i < setsLength; i++){
-                var setName = $(sets[i]).text();
-                var setId = $(sets[i]).val();
-                setList.push({ id: setId, order: i, name: setName });
-            }
+      for(var i = 0; i < setsLength; i++){
+        var setName = $(sets[i]).text();
+        var setId = $(sets[i]).val();
+        setList.push({ id: setId, order: i, name: setName });
+      }
 
-            res.send(JSON.stringify(setList));
+      res.send(JSON.stringify(setList));
+    }
+  });
+});
+
+app.post('/tcgCard', function(req, res) {
+  var setName = req.body.set;
+  var cardName = req.body.card;
+  var url = 'https://shop.tcgplayer.com/magic/' + setName + '/' + cardName;
+
+  console.log(url);
+
+  request(url, function(error, response, html){
+    if(!error){
+      function sleep(milliseconds) {
+        var start = new Date().getTime();
+        for (var i = 0; i < 1e7; i++) {
+          if ((new Date().getTime() - start) > milliseconds){
+            break;
+          }
         }
-    });
+      }
+
+      var $ = cheerio.load(html);
+      var cardPrice = $('.price-point__data').first().text();
+      console.log(JSON.stringify(cardPrice));
+
+      var returnCard = { name: cardName, price: cardPrice };
+      res.send(JSON.stringify(returnCard));
+    }
+  });
 });
 
 app.post('/msmPriceCards', function(req, res) {
-    var setId = req.body.id;
-    var url = 'http://www.msmmm.com/pricelist';
-    var cardList = [];
+  var setId = req.body.id;
+  var url = 'http://www.msmmm.com/pricelist';
+  var cardList = [];
 
-    var queryString = '?field_magic_color_nid_op=in'
+  var queryString = '?field_magic_color_nid_op=in'
         + '&field_magic_price_value_op=%3D'
         + '&field_magic_price_value%5Bvalue%5D='
         + '&field_magic_price_value%5Bmin%5D='
@@ -102,41 +130,41 @@ app.post('/msmPriceCards', function(req, res) {
         + '&title='
         + '&items_per_page=200';
 
-    var urlString = url + queryString;
-    console.log(urlString);
+  var urlString = url + queryString;
+  console.log(urlString);
 
-    request(urlString, function(error, response, html){
-        if(!error){
-            var $ = cheerio.load(html);
+  request(urlString, function(error, response, html){
+    if(!error){
+      var $ = cheerio.load(html);
 
-            var content = $('.views-table');
-            var cards = content.find('tr');
-            var cardsLength = cards.length;
+      var content = $('.views-table');
+      var cards = content.find('tr');
+      var cardsLength = cards.length;
 
-            for(var y = 0; y < cardsLength; y++){
-                var cols = $(cards[y]).find('td');
-                var colLength = cols.length;
+      for(var y = 0; y < cardsLength; y++){
+        var cols = $(cards[y]).find('td');
+        var colLength = cols.length;
 
-                var cardName = $(cols[0]).text().replace(/\r?\n|\r/, "").trim();
-                var cardColor = $(cols[1]).text().replace(/\r?\n|\r/, "").trim();
-                var cardCost = $(cols[2]).text().replace(/\r?\n|\r/, "").trim();
-                var cardRarity = $(cols[3]).text().replace(/\r?\n|\r/, "").trim();
-                var cardSet = $(cols[4]).text().replace(/\r?\n|\r/, "").trim();
-                var cardWantList = $(cols[5]).text().replace(/\r?\n|\r/, "").trim();
+        var cardName = $(cols[0]).text().replace(/\r?\n|\r/, "").trim();
+        var cardColor = $(cols[1]).text().replace(/\r?\n|\r/, "").trim();
+        var cardCost = $(cols[2]).text().replace(/\r?\n|\r/, "").trim();
+        var cardRarity = $(cols[3]).text().replace(/\r?\n|\r/, "").trim();
+        var cardSet = $(cols[4]).text().replace(/\r?\n|\r/, "").trim();
+        var cardWantList = $(cols[5]).text().replace(/\r?\n|\r/, "").trim();
 
-                if (cardSet != ""){
-                    var card = { name: cardName, color: cardColor, cost: cardCost, rarity: cardRarity, set: cardSet, wantList: cardWantList };
-                    console.log(JSON.stringify(card));
+        if (cardSet != ""){
+          var card = { name: cardName, color: cardColor, cost: cardCost, rarity: cardRarity, set: cardSet, wantList: cardWantList };
+          console.log(JSON.stringify(card));
 
-                    cardList.push(card);
-                }
-            }
-
-            res.send(JSON.stringify(cardList));
+          cardList.push(card);
         }
-    });
+      }
+
+      res.send(JSON.stringify(cardList));
+    }
+  });
 });
 
 app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
+  console.log('Node app is running on port', app.get('port'));
 });
--- public/msm/tradeViewmodel.js
+++ public/msm/tradeViewmodel.js
@@ -1,177 +1,199 @@
 var Card = function() {
-    var self = this;
+  var self = this;
 
-    self.Name = "";
-    self.Set = "";
-    self.Cost = 0;
+  self.Name = "";
+  self.Set = "";
+  self.Cost = 0;
+  self.TcgPrice = ko.observable(0);
 };
 
 var Set = function() {
-    var self = this;
+  var self = this;
 
-    self.Id = 0;
-    self.Order = 0;
-    self.Name = "";
+  self.Id = 0;
+  self.Order = 0;
+  self.Name = "";
 };
 
 var ViewModel = function() {
-    var self = this;
+  var self = this;
 
-    self.ShopCards = ko.observableArray([]);
-    self.CustCards = ko.observableArray([]);
-    self.Sets = ko.observableArray([]);
-    self.CardFilter = ko.observable("");
-    self.SetFilter = ko.observable("");
-    self.CostFilter = ko.observable(0);
-    self.SelectedOperator = ko.observable();
+  self.ShopCards = ko.observableArray([]);
+  self.CustCards = ko.observableArray([]);
+  self.Sets = ko.observableArray([]);
+  self.CardFilter = ko.observable("");
+  self.SetFilter = ko.observable("");
+  self.CostFilter = ko.observable(0);
+  self.SelectedOperator = ko.observable();
 
-    self.Operators = ko.observableArray([]);
+  self.Operators = ko.observableArray([]);
 
-    self.FilteredCards = ko.computed(function() {
-        return ko.utils.arrayFilter(self.ShopCards(), function(card) {
-            return (card.Name.indexOf(self.CardFilter()) > -1
-                    && (card.Set == self.SelectedSet().Name || self.SelectedSet().Id == 0)
-                    && self.CompareCost(Number(card.Cost)));
-        });
+  self.FilteredCards = ko.computed(function() {
+    return ko.utils.arrayFilter(self.ShopCards(), function(card) {
+      return (card.Name.indexOf(self.CardFilter()) > -1
+              && (card.Set == self.SelectedSet().Name || self.SelectedSet().Id == 0)
+              && self.CompareCost(Number(card.Cost)));
     });
+  });
 
-    self.CompareCost = function(cardCost){
-        var operator = self.SelectedOperator().name;
-        var filterCost = Number(self.CostFilter());
-        if (operator == ">="){
-            return cardCost >= filterCost;
-        } else {
-            return cardCost <= filterCost;
+  self.CompareCost = function(cardCost){
+    var operator = self.SelectedOperator().name;
+    var filterCost = Number(self.CostFilter());
+    if (operator == ">="){
+      return cardCost >= filterCost;
+    } else {
+      return cardCost <= filterCost;
+    }
+  };
+
+  self.PurchaseTotal = ko.pureComputed(function() {
+    var total = 0;
+    var cards = self.CustCards();
+
+    for(var i = 0; i < cards.length; i++) {
+      total += Number(cards[i].Cost);
+    }
+
+    return total;
+  }, this);
+
+  self.SelectedSet = ko.observable(new Set());
+
+  // Methods
+  self.Init = function() {
+    self.SetOperators();
+    self.GetSets();
+  };
+
+  self.SetOperators = function(){
+    self.Operators.push({ id: 0, name: ">=" });
+    self.Operators.push({ id: 0, name: "<=" });
+  };
+
+  self.SetShopCard = function(cardData) {
+    var newCard = new Card();
+
+    if (cardData) {
+      newCard.Name = cardData.name;
+      newCard.Set = cardData.set;
+      newCard.Cost = cardData.cost.replace("$", "");
+    }
+
+    self.ShopCards.push(newCard);
+  };
+
+  self.SetSet = function(setData) {
+    var newSet = new Set();
+
+    if (setData) {
+      newSet.Id = setData.id;
+      newSet.Order = setData.order;
+      newSet.Name = setData.name;
+    }
+
+    self.Sets.push(newSet);
+  };
+
+  self.AddCard = function(card)
+  {
+    self.CustCards.push(card);
+  };
+
+  self.RemoveCard = function(card)
+  {
+    self.CustCards.remove(card);
+  };
+
+  // API Methods
+  self.GetCardTcgPrice = function(givenCard) {
+    var jsonData = {
+      card: givenCard.Name.replace(/ /g, "-"),
+      set: givenCard.Set.replace(/ /g, "-")
+    };
+
+    $.ajax({
+      url: "/tcgCard",
+      type: "POST",
+      dataType: "json",
+      contentType: "application/json",
+      data: JSON.stringify(jsonData),
+      success: function (data) {
+        givenCard.TcgPrice(data.price);
+      },
+      error: function (request, error) {
+        //alert("An error occured!");
+      }
+    });
+  };
+
+  self.GetShopCards = function() {
+    $.ajax({
+      url: "/msmWantCards",
+      type: "GET",
+      dataType: "json",
+      success: function(data) {
+        self.ProcessArray(data, self.SetShopCard, self.Done);
+      },
+      error: function(request, error) {
+        alert(error.responseJSON.Message);
+      }
+    });
+  };
+
+  self.Done = function() {
+    console.log("I finished");
+  };
+
+  self.GetSets = function() {
+    var allSets = new Set();
+    allSets.Id = 0;
+    allSets.Order = -1;
+    allSets.Name = "All";
+
+    $.ajax({
+      url: "/msmSets",
+      type: "GET",
+      dataType: "json",
+      success: function(data) {
+        self.Sets.push(allSets);
+        for(var i = 0; i < data.length; i ++){
+          self.SetSet(data[i]);
         }
-    };
 
-    self.PurchaseTotal = ko.pureComputed(function() {
-        var total = 0;
-        var cards = self.CustCards();
+        self.GetShopCards();
+      },
+      error: function(request, error) {
+        alert(error.responseJSON.Message);
+      }
+    });
+  };
 
-        for(var i = 0; i < cards.length; i++) {
-            total += Number(cards[i].Cost);
-        }
+  self.ProcessArray = function(data, handler, callback) {
+    var maxtime = 100;         // chunk processing time
+    var delay = 20;            // delay between processes
+    var queue = data.concat(); // clone original array
 
-        return total;
-    }, this);
+    setTimeout(function() {
 
-    self.SelectedSet = ko.observable(new Set());
+      var endtime = +new Date() + maxtime;
 
-    // Methods
-    self.Init = function() {
-        self.SetOperators();
-        self.GetSets();
-    };
+      do {
+        handler(queue.shift());
+      } while (queue.length > 0 && endtime > +new Date());
 
-    self.SetOperators = function(){
-        self.Operators.push({ id: 0, name: ">=" });
-        self.Operators.push({ id: 0, name: "<=" });
-    };
+      if (queue.length > 0) {
+        setTimeout(arguments.callee, delay);
+      }
+      else {
+        if (callback) callback();
+      }
 
-    self.SetShopCard = function(cardData) {
-        var newCard = new Card();
+    }, delay);
+  };    // end of ProcessArray function
 
-        if (cardData) {
-            newCard.Name = cardData.name;
-            newCard.Set = cardData.set;
-            newCard.Cost = cardData.cost.replace("$", "");
-        }
-
-        self.ShopCards.push(newCard);
-    };
-
-    self.SetSet = function(setData) {
-        var newSet = new Set();
-
-        if (setData) {
-            newSet.Id = setData.id;
-            newSet.Order = setData.order;
-            newSet.Name = setData.name;
-        }
-
-        self.Sets.push(newSet);
-    };
-
-    self.AddCard = function(card)
-    {
-        self.CustCards.push(card);
-    };
-
-    self.RemoveCard = function(card)
-    {
-        self.CustCards.remove(card);
-    };
-
-    // API Methods
-    self.GetShopCards = function() {
-        $.ajax({
-            url: "/msmWantCards",
-            type: "GET",
-            dataType: "json",
-            success: function(data) {
-                self.ProcessArray(data, self.SetShopCard, self.Done);
-            },
-            error: function(request, error) {
-                alert(error.responseJSON.Message);
-            }
-        });
-    };
-
-    self.Done = function() {
-        console.log("I finished");
-    };
-
-    self.GetSets = function() {
-        var allSets = new Set();
-        allSets.Id = 0;
-        allSets.Order = -1;
-        allSets.Name = "All";
-
-        $.ajax({
-            url: "/msmSets",
-            type: "GET",
-            dataType: "json",
-            success: function(data) {
-                self.Sets.push(allSets);
-                for(var i = 0; i < data.length; i ++){
-                    self.SetSet(data[i]);
-                }
-
-                self.GetShopCards();
-            },
-            error: function(request, error) {
-                alert(error.responseJSON.Message);
-            }
-        });
-    };
-
-    self.ProcessArray = function(data, handler, callback) {
-        var maxtime = 100;         // chunk processing time
-        var delay = 20;            // delay between processes
-        var queue = data.concat(); // clone original array
-
-        setTimeout(function() {
-
-            var endtime = +new Date() + maxtime;
-
-            do {
-                handler(queue.shift());
-            } while (queue.length > 0 && endtime > +new Date());
-
-            if (queue.length > 0) {
-                setTimeout(arguments.callee, delay);
-            }
-            else {
-                if (callback) callback();
-            }
-
-        }, delay);
-    };    // end of ProcessArray function
-
-    self.Init();
+  self.Init();
 };
 
 $(document).ready(function () {
-    ko.applyBindings(new ViewModel());
+  ko.applyBindings(new ViewModel());
 });
